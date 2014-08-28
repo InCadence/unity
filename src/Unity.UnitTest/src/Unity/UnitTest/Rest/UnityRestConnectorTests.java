fb0de07d2101ed8c9774454743bdac42de68251f
Derek Clemenzi <dclemenzi@incadencecorp.com>
2014-08-28 12:21:33 -0400

    Updated styles

--- src/Unity.UnitTest/src/Unity/UnitTest/Rest/UnityRestConnectorTests.java
+++ src/Unity.UnitTest/src/Unity/UnitTest/Rest/UnityRestConnectorTests.java
@@ -20,93 +20,118 @@ import unity.core.runtime.CallResult.CallResults;
 public class UnityRestConnectorTests {
 
 	private static String _testStringValue;
-	private static int _testIntValue; 
-	
+	private static int _testIntValue;
+
 	@BeforeClass
 	public static void Initialize() {
-		
+
 		Random randomGenerator = new Random();
-		
+
 		int randomInt = randomGenerator.nextInt(100);
-		
+
 		// Create Test Data
 		_testStringValue = "Matthew" + randomInt;
 		_testIntValue = randomInt;
-		
+
 		RestConfigConnector.initialize("localhost", 8080);
-		
+
 		TestSettings.Initialize(new RestConfigurationsConnector("localhost", 8080));
-		
+
 	}
-	
+
 	@Test
 	public void TestSetSettings() {
 
-		try{
+		try {
 
-			String value; 
+			String value;
 			int intValue;
-			
-			//REST Configuration
+
+			// REST Configuration
 			RestConfigConnector.setSetting("app.config", "app/section1/firstname", _testStringValue, SettingType.stString);
-			RestConfigConnector.setSetting("app.config", "app/section1/random", Integer.toString(_testIntValue), SettingType.stInteger);
-			
- 			value = RestConfigConnector.getSetting("app.config", "app/section1/firstname", "", SettingType.stString, false);
-			intValue = Integer.parseInt(RestConfigConnector.getSetting("app.config", "app/section1/random", "", SettingType.stInteger, false));
-			
+			RestConfigConnector.setSetting("app.config", "app/section1/random", Integer.toString(_testIntValue),
+					SettingType.stInteger);
+
+			value = RestConfigConnector.getSetting("app.config", "app/section1/firstname", "", SettingType.stString, false);
+			intValue = Integer.parseInt(RestConfigConnector.getSetting("app.config", "app/section1/random", "",
+					SettingType.stInteger, false));
+
 			assertTrue("Mismatch", value.equals(_testStringValue));
 			assertTrue("Mismatch", _testIntValue == intValue);
 
-			
-		} catch (Exception ex) {
+		}
+		catch (Exception ex) {
 			fail(ex.getMessage());
 		}
-		
+
 	}
-	
+
 	@Test
 	public void TestGetSettings() {
-		
-		String Stringvalue; 
-		int intValue;
-		
-		Stringvalue = RestConfigConnector.getSetting("app.config", "app/section1/firstname", "", SettingType.stString, false);
-		intValue = Integer.parseInt(RestConfigConnector.getSetting("app.config", "app/section1/random", "", SettingType.stInteger, false));
-			
-		assertTrue("Mismatch", Stringvalue.equals(_testStringValue));
-		assertTrue("Mismatch", _testIntValue == intValue);
-		
+
+		try {
+
+			String Stringvalue;
+			int intValue;
+
+			Stringvalue = RestConfigConnector.getSetting("app.config", "app/section1/firstname", "", SettingType.stString,
+					false);
+			intValue = Integer.parseInt(RestConfigConnector.getSetting("app.config", "app/section1/random", "",
+					SettingType.stInteger, false));
+
+			assertTrue("Mismatch", Stringvalue.equals(_testStringValue));
+			assertTrue("Mismatch", _testIntValue == intValue);
+
+		}
+		catch (Exception ex) {
+			fail(ex.getMessage());
+		}
+
 	}
-	
+
 	@Test
 	public void TestAddLogEntry() {
-		CallResult rst = new CallResult(CallResults.SUCCESS);
-		RestConfigConnector.log("TestLogName.log", rst.toXML(true));
+
+		try {
+
+			CallResult rst = new CallResult(CallResults.SUCCESS);
+			RestConfigConnector.log("TestLogName.log", rst.toXML(true));
+
+		}
+		catch (Exception ex) {
+			fail(ex.getMessage());
+		}
 	}
-	
+
 	@Test
 	public void TestSettingsClass() {
-		
-		assertTrue("Failed Set", TestSettings.setTestStringSetting(_testStringValue));
-		assertTrue("Failed Set", TestSettings.setTestIntSetting(_testIntValue));
-		assertTrue("Failed Set", TestSettings.setTestBooleanSetting(true));
 
-		TestSettings.ClearCache();
-		
-		assertTrue("Mismatch", _testStringValue == TestSettings.getTestStringSetting());
-		assertTrue("Mismatch", _testIntValue == TestSettings.getTestIntSetting());
-		assertTrue("Mismatch", TestSettings.getTestBooleanSetting());
+		try {
+			assertTrue("Failed Set", TestSettings.setTestStringSetting(_testStringValue));
+			assertTrue("Failed Set", TestSettings.setTestIntSetting(_testIntValue));
+			assertTrue("Failed Set", TestSettings.setTestBooleanSetting(true));
 
-		assertTrue("Failed Set", TestSettings.setTestBooleanSetting(false));
+			TestSettings.ClearCache();
 
-		TestSettings.ClearCache();
+			assertTrue("Mismatch", _testStringValue == TestSettings.getTestStringSetting());
+			assertTrue("Mismatch", _testIntValue == TestSettings.getTestIntSetting());
+			assertTrue("Mismatch", TestSettings.getTestBooleanSetting());
 
-		assertFalse("Mismatch", TestSettings.getTestBooleanSetting());
+			assertTrue("Failed Set", TestSettings.setTestBooleanSetting(false));
+
+			TestSettings.ClearCache();
+
+			assertFalse("Mismatch", TestSettings.getTestBooleanSetting());
+
+		}
+		catch (Exception ex) {
+			fail(ex.getMessage());
+		}
 	}
 
 	@After
 	public void Finalize() {
-		
+
 	}
-	
+
 }
